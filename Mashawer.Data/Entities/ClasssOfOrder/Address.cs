d34363d94eed1a20c78ae1c61519d0a538b1ea97
Abdullah Al-Najjar <105871514+Abdullahkamalalnajjar@users.noreply.github.com>
2025-08-12 20:19:25 +0300

    last update

--- Mashawer.Data/Entities/ClasssOfOrder/Address.cs
+++ Mashawer.Data/Entities/ClasssOfOrder/Address.cs
@@ -4,7 +4,6 @@
     {
         public string StreetName { get; set; }           // اسم الشارع
         public string EntranceNumber { get; set; }       // رقم المدخل
-        public string AddressDetails { get; set; }       // تفاصيل إضافية
         public string PhoneNumber { get; set; }        // رقم الهاتف    
     }
 
